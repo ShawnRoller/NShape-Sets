0918542d22d522196a513e410a361f2bae0c448c
SRoller <sroller@justfab.com>
2019-12-02 16:00:58 -0500

    organizes some xcode project files

---
+++ NShapeSets/NShapeSets/Controller/HostingController.swift
@@ -0,0 +1,9 @@
+//
+//  HostingController.swift
+//  NShapeSets
+//
+//  Created by Shawn Roller on 12/2/19.
+//  Copyright © 2019 offensively-bad. All rights reserved.
+//
+
+import Foundation
---
+++ NShapeSets/NShapeSets/Views/SetupView.swift
@@ -0,0 +1,9 @@
+//
+//  SetupView.swift
+//  NShapeSets
+//
+//  Created by Shawn Roller on 12/2/19.
+//  Copyright © 2019 offensively-bad. All rights reserved.
+//
+
+import Foundation
