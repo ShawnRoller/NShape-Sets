67618398dd2e049e8adb3ff1fa8d08c3659098e9
SRoller <sroller@justfab.com>
2019-12-13 18:04:15 -0500

    Adds fonts and assets

---
+++ NShapeSets/NShapeSets/Helpers/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  NShapeSets
+//
+//  Created by Shawn Roller on 12/13/19.
+//  Copyright © 2019 offensively-bad. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct Theme {
+    static let settingsFont = UIFont(name: "Gotham-Book", size: 36)
+    static let header1Font = UIFont(name: "Gotham-Book", size: 60)
+    static let header2Font = UIFont(name: "Gotham-Book", size: 45)
+    
+    static let wTypeFont = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let wSettingsFont = UIFont.systemFont(ofSize: 26, weight: .regular)
+    static let wHeader1Font = UIFont.systemFont(ofSize: 38, weight: .regular)
+    static let wHeader2Font = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let wHeader3Font = UIFont.systemFont(ofSize: 17, weight: .regular)
+}
