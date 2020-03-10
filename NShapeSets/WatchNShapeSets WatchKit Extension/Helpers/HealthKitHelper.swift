f3a8d3ae40a238e4c488631239120c147ce182ec
SRoller <sroller@justfab.com>
2020-03-10 08:06:54 -0400

    Changes to healthkithelper
    Adds delegate protocols subs for delegate methods

--- NShapeSets/WatchNShapeSets WatchKit Extension/Helpers/HealthKitHelper.swift
+++ NShapeSets/WatchNShapeSets WatchKit Extension/Helpers/HealthKitHelper.swift
@@ -6,14 +6,59 @@
 //  Copyright © 2020 offensively-bad. All rights reserved.
 //
 
+import WatchKit
 import Foundation
 import HealthKit
 
-struct HealthKitHelper {
+class HealthKitHelper: WKInterfaceController, HKWorkoutSessionDelegate, HKLiveWorkoutBuilderDelegate {
     
-    private var healthStore = HKHealthStore()
-    private var session: HKWorkoutSession
-    private var builder: HKWorkoutBuilder
+    private var healthStore: HKHealthStore!
+    private var config: HKWorkoutConfiguration!
+    private var session: HKWorkoutSession!
+    private var builder: HKLiveWorkoutBuilder!
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+        // Dispatch to main, because we are updating the interface.
+        DispatchQueue.main.async {
+            // update the UI based on state
+        }
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        DispatchQueue.main.async {
+            // update the UI based on state
+        }
+    }
+    
+    func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
+        for type in collectedTypes {
+            guard let quantityType = type as? HKQuantityType else {
+                return // Nothing to do.
+            }
+            
+            /// - Tag: GetStatistics
+//            let statistics = workoutBuilder.statistics(for: quantityType)
+//            let label = labelForQuantityType(quantityType)
+//
+//            updateLabel(label, withStatistics: statistics)
+        }
+    }
+    
+    func workoutBuilderDidCollectEvent(_ workoutBuilder: HKLiveWorkoutBuilder) {
+        // Retreive the workout event.
+        guard let workoutEventType = workoutBuilder.workoutEvents.last?.type else { return }
+        
+        // Update the timer based on the event received.
+//        switch workoutEventType {
+//        case .pause: // The user paused the workout.
+//            setDurationTimerDate(.paused)
+//        case .resume: // The user resumed the workout.
+//            setDurationTimerDate(.running)
+//        default:
+//            return
+//
+//        }
+    }
     
     func setupHK() {
         let typesToShare: Set = [HKQuantityType.workoutType()]
@@ -52,6 +97,7 @@ struct HealthKitHelper {
         builder.beginCollection(withStart: Date()) { (success, error) in
             guard success else {
                 // handle error
+                return
             }
             
             // session has started
