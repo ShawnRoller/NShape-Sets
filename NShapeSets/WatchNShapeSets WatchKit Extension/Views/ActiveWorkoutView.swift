2b14854c9cf2c0113b7189e146381fde40c69429
Shawn Roller <sroller@techstyle.com>
2020-03-28 12:39:13 -0400

    Adds total time to watch completed workout alert,

--- NShapeSets/WatchNShapeSets WatchKit Extension/Views/ActiveWorkoutView.swift
+++ NShapeSets/WatchNShapeSets WatchKit Extension/Views/ActiveWorkoutView.swift
@@ -48,7 +48,8 @@ struct ActiveWorkoutView: View {
         }
         .alert(isPresented: $showingAlert) {
             self.timer.stopTimeTracking()
-            return Alert(title: Text("Workout complete!"), message: Text("You completed all sets in \(self.timer.totalTime) seconds!"), dismissButton: .default(Text("OK"), action: {
+            let totalTime = TimeHelper.getTimeFromSeconds(self.timer.totalTime)
+            return Alert(title: Text("Workout complete!"), message: Text("You completed all sets in \(totalTime)!"), dismissButton: .default(Text("OK"), action: {
                 /// TODO: need to add a delay here as the "start" button doesn't work if there's no delay.  This appears to be a bug with how the callback is handled and may be fixed in a new version of swiftui
                 DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
                     self.goBack()
