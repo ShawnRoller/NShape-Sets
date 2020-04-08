61ec8537174a15ca640a5dad02adc32e65711072
Shawn Roller <sroller@techstyle.com>
2020-04-07 21:29:03 -0400

    Fixes a bug where the total time is displayed in seconds instead of minutes:seconds.

--- NShapeSets/NShapeSets/Helpers/TimeHelper.swift
+++ NShapeSets/NShapeSets/Helpers/TimeHelper.swift
@@ -11,9 +11,9 @@ import Foundation
 struct TimeHelper {
     
     static func getTimeFromSeconds(_ seconds: Int) -> String {
-        let minutes = floor(Double(seconds) / 60)
+        let minutes = seconds / 60 % 60
         let seconds = seconds % 60
-        let time = String(format: "%02d:%02d", minutes, seconds)
+        let time = String(format:"%02i:%02i", minutes, seconds)
         return time
     }
     
